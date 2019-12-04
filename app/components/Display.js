e3c0115fb9b5615ea26414d432c0abad970f2f9e
Sabelle Huang <sabelle2@illinois.edu>
2019-12-03 19:29:40 -0600

    hates css formatting

--- app/components/Display.js
+++ app/components/Display.js
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 import styles from './Display.css';
 import routes from '../constants/routes';
 
-// import ap-output from './ap-output.png';
-// import lat-output from './lat-output.png';
+import apLabeled1 from './ap-labeled1.png';
+import apLabeled2 from './ap-labeled2.png';
+import latLabeled1 from './lat-labeled1.png';
+import latLabeled2 from './lat-labeled2.png';
+
+import apPre from './ap-pre.png';
+import apPost from './ap-post.png';
+import apOverlay from './ap-overlay.png';
+import latPre from './lat-pre.png';
+import latPost from './lat-post.png';
+import latOverlay from './lat-overlay.png';
 
 type Props = {};
 
@@ -13,17 +22,19 @@ export default class Display extends Component<Props> {
 
   render() {
     return (
-      <div className={styles.backButton} data-tid="backButton">
-        <Link to={routes.HOME}>
-          <i className="fa fa-arrow-left fa-3x" />
-        </Link>
+      <div>
+        <div className={styles.backButton} data-tid="backButton">
+          <Link to={routes.HOME}>
+            <i className="fa fa-arrow-left fa-3x" />
+          </Link>
+        </div>
+        
+        <div className={styles.images} data-tid="images">
+          <img src={apPre} />
+          <img src={apOverlay} />
+          <img src={apPost} />
+        </div>
       </div>
-
-      // <div>
-      //   <img src={ap-output}>
-      //   <img src={lat-output}>
-      // </div>
-
     );
   }
 }
