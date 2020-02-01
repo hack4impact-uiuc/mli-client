26817eb9d9481a6bb355f3f0f39ce9d3a04a324c
arpanlaha <32147742+arpanlaha@users.noreply.github.com>
2020-02-01 16:16:38 -0600

    fix lambda url for both overlay and annotate functions

--- app/utils/ApiWrapper.js
+++ app/utils/ApiWrapper.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const OVERLAY_URL =
-  'https://vilx2sy763.execute-api.us-east-1.amazonaws.com/Prod';
-const ANNOTATE_URL =
-  'https://fih642j6q0.execute-api.us-east-1.amazonaws.com/Prod/annotate';
+const BACKEND_URL =
+  'https://fih642j6q0.execute-api.us-east-1.amazonaws.com/Prod';
 const API_TOKEN = 'miaomiaoVERYsecurityMIAO';
 
 export const getOverlay = (preImage, postImage, noiseReduction) => {
   return axios({
     method: 'post',
-    url: `${OVERLAY_URL}`,
+    url: `${BACKEND_URL}/overlay`,
     data: { preImage, postImage, noiseReduction },
     headers: { 'Content-Type': 'application/json', 'x-api-key': API_TOKEN }
   })
@@ -23,7 +21,7 @@ export const getOverlay = (preImage, postImage, noiseReduction) => {
 export const getAnnotate = () => {
   return axios({
     method: 'get',
-    url: `${ANNOTATE_URL}`,
+    url: `${BACKEND_URL}/annotate`,
     headers: { 'Content-Type': 'application/json', 'x-api-key': API_TOKEN }
   })
     .then(response => ({
