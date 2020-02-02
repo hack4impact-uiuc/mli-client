1c4ee8397ed4adba51dd4a55883f4ba7e138276b
arpanlaha <32147742+arpanlaha@users.noreply.github.com>
2020-02-02 14:40:15 -0600

    add description to overlay

--- app/components/Overlayed.js
+++ app/components/Overlayed.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styles from './ImageDisplays.css';
 import routes from '../constants/routes';
 import { connect } from 'react-redux';
+import './Home.css';
 
 type Props = {};
 
@@ -43,6 +44,19 @@ class Overlayed extends Component<Props> {
               Labeled Images
             </button>
           </Link>
+        </div>
+        <div className={styles.centered} data-tid="centered">
+          <h4>
+            The pre and post images are colored green and magenta, respectively,
+            so that when they are overlaid the resulting pixel is white.
+            Additionally, the pre image is cropped and warped so that its
+            overlay most closely matches the post image.
+          </h4>
+          <h4>
+            As such, white pixels in the overlapped image indicate an overlap of
+            the pre and post image, while a colored pixel represents only the
+            presence of the correspondingly-colored image.
+          </h4>
         </div>
         <div className={styles.iframe} data-tid="iframe">
           {[
